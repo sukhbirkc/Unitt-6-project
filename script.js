443343e8278f4c66a5d239e298ac4cbedf99e8f1
sukhbirkc <97969645+sukhbirkc@users.noreply.github.com>
2022-02-03 10:57:32 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,72 @@
+// Guided Practice
+$(".reply-yes").click(function() {  
+    $(".homers-reply").text("Woo Hoo!");
+    $(".reply-yes").css("background-color", "yellow");
+});
+
+//Independent Practice
+$(".replyNo").click(function() {  
+      
+    
+});
+
+$(".yes").click(function() {
+  $(".img").slideUp();   
+});
+
+$(".yes").click(function() {
+  $(".yes").hide();   
+});
+$(".yes").click(function() {
+  $(".no").hide();   
+});
+$(".reply").hide();
+$(".yes").click(function() {
+  $(".reply").show();   
+});
+$(".one").hide();
+$(".yes").click(function() {
+  $(".one").show();   
+});
+$(".two").hide();
+$(".yes").click(function() {
+  $(".two").show();   
+});
+$(".three").hide();
+$(".replyy").hide();
+$(".four").hide();
+$(".replyyy").hide();
+
+$(".one").dblclick(function() {
+  $(".three").show();   
+});
+$(".two").dblclick(function() {
+  $(".four").show();   
+});
+$(".one").dblclick(function() {
+  $(".two").hide();   
+});
+$(".one").dblclick(function() {
+  $(".one").hide();   
+});
+$(".two").dblclick(function() {
+  $(".one").hide();   
+});
+$(".two").dblclick(function() {
+  $(".two").hide();   
+});
+$(".one").dblclick(function() {
+  $(".reply").hide();   
+});
+$(".two").dblclick(function() {
+  $(".reply").hide();   
+});
+$(".one").dblclick(function() {
+  $(".replyy").show();   
+});
+$(".two").dblclick(function() {
+  $(".replyy").hide();   
+});
+$(".two").dblclick(function() {
+  $(".replyyy").show();   
+});
